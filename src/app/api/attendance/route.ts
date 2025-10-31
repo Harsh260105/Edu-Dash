45916659418b86ef240bbcd70810f7f5be8e722f
Harsh260105 <harshpatel17079@gmail.com>
2025-10-31 14:30:47 +0530

    feat: set dynamic routing for attendance, classes, fees, lessons, results, and my-lessons APIs

--- src/app/api/attendance/route.ts
+++ src/app/api/attendance/route.ts
@@ -2,6 +2,8 @@ import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 import { auth } from "@clerk/nextjs/server";
 
+export const dynamic = "force-dynamic";
+
 export async function POST(request: NextRequest) {
   try {
     const { userId, sessionClaims } = auth();
